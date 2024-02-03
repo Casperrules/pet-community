70c6108785e55f98fa61f953b8c6de6fb03e6619
System Administrator <root@adarshs-MacBook-Air.local>
2024-02-03 18:22:43 +0530

    added mobile menu

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -3,20 +3,22 @@ import React, { useState } from "react";
 import { IoMdMenu } from "react-icons/io";
 import { FaUser } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
 import Input from "../inputs/input";
 
 const Header = () => {
   const [input, setInput] = useState("");
+  const [openMenu, setOpenMenu] = useState(false);
   return (
     <div className="fixed h-[60px] md:h-[80px] top-0 z-50 flex w-full bg-[#4d4dcf] p-4 text-[#fff]">
       <div className="small-appbar flex w-full justify-between">
         <div
           className="menu-button visible cursor-pointer lg:hidden"
           onClick={() => {
-            console.log("open the menu");
+            setOpenMenu(true);
           }}
         >
-          <IoMdMenu size={25} />
+          {<IoMdMenu size={25} />}
         </div>
         <div className="app-name self-center font-bold cursor-pointer">
           Woof&Trill
@@ -51,6 +53,31 @@ const Header = () => {
           <FaUser size={25} />
         </div>
       </div>
+      {openMenu && (
+        <div className="menu-sidebar absolute top-0 left-0 w-[250px] p-2 bg-[#fff] h-screen border-r-2 shadow-lg">
+          <div className="menu-header flex p-2 text-[#000] justify-end border-b ">
+            <div
+              className="cursor-pointer"
+              onClick={() => {
+                setOpenMenu(false);
+              }}
+            >
+              <IoMdClose size={25} />
+            </div>
+          </div>
+          <div className="menu-options flex flex-col">
+            <div className="menu-items m-2 p-1 text-[#000] border border-solid hover:bg-[#4d4dcf94] hover:text-[#fff] hover:shadow-md cursor-pointer rounded-full">
+              Community
+            </div>
+            <div className="menu-items m-2 p-1 text-[#000] border border-solid hover:bg-[#4d4dcf94] hover:text-[#fff] hover:shadow-md cursor-pointer rounded-full">
+              About
+            </div>
+            <div className="menu-items m-2 p-1 text-[#000] border border-solid hover:bg-[#4d4dcf94] hover:text-[#fff] hover:shadow-md cursor-pointer rounded-full">
+              Contact Us
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
